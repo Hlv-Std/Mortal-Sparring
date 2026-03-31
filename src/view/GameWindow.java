fde2e7527f9e5de924bc009f91046e2ef126aeb4
Helvetica-Standard <genangeliludovico26+helvetica@gmail.com>
2026-03-31 11:57:38 +0200

    feat: load player1

--- src/view/GameWindow.java
+++ src/view/GameWindow.java
@@ -1,13 +1,21 @@
 package view;
 
+import controller.PlayerLoader;
+import model.Player;
+
 import javax.swing.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.WindowEvent;
+import java.nio.file.Path;
 
 public class GameWindow extends JFrame implements KeyListener {
     private static final int DEF_WIDTH = 500;
     private static final int DEF_HEIGHT = 400;
+
+    private GamePanel gamePanel;
+    private Player player1;
+    private PlayerLoader player1Loader;
 
     public GameWindow(){
         super("Mortal Sparring");
@@ -24,7 +32,15 @@ public class GameWindow extends JFrame implements KeyListener {
     }
 
     private void initComponents(){
+        // Preapre the player then add it to the game
+        player1 = new Player("Fazz");
+        player1Loader = new PlayerLoader(player1, Path.of("./src/resources/fazz"));
+        player1Loader.loadAnimations();
+        player1.x = (double) getWidth() /2;
+        player1.y = (double) getHeight() /2;
 
+        gamePanel = new GamePanel(player1);
+        setContentPane(gamePanel);
     }
 
     private void loadLayout(){
