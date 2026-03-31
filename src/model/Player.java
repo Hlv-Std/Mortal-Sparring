9968df720422615509c14390a319bbe5cf64a34e
Helvetica-Standard <genangeliludovico26+helvetica@gmail.com>
2026-03-31 11:57:06 +0200

    feat(player): implement ccoordinates

--- src/model/Player.java
+++ src/model/Player.java
@@ -1,15 +1,25 @@
 package model;
 
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-public class Player {
+public class Player extends Vector2{
     private String name;
     private HashMap<String, List<BufferedImage>> animations;
 
     public Player(String name){
+        super();
         this.name = name;
         animations = new HashMap<>();
+        // Set up player animations
+        animations.put("Idle", new ArrayList<>());
+    }
+
+    public String getName(){ return name; }
+
+    public List<BufferedImage> getFrames(String animationName){
+        return animations.get(animationName);
     }
 }
