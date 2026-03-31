139c92713fd83bc3916e03f73275064b04667c21
Helvetica-Standard <genangeliludovico26+helvetica@gmail.com>
2026-03-31 19:00:39 +0200

    feat(player): add some state varaibles and methods for movement

--- src/model/Player.java
+++ src/model/Player.java
@@ -7,19 +7,57 @@ import java.util.List;
 
 public class Player extends Vector2{
     private String name;
+    private String animationState;
     private HashMap<String, List<BufferedImage>> animations;
+    public double velX;
+    public double velY;
+    private boolean isInAir;
+    private boolean isFalling;
+    private boolean isDucking;
 
     public Player(String name){
         super();
         this.name = name;
+        velX = 0;
+        velY = 0;
+        animationState = PlayerAnimationState.IDLE;
         animations = new HashMap<>();
         // Set up player animations
-        animations.put("Idle", new ArrayList<>());
+        animations.put(PlayerAnimationState.IDLE, new ArrayList<>());
     }
 
     public String getName(){ return name; }
-
     public List<BufferedImage> getFrames(String animationName){
         return animations.get(animationName);
     }
+    public String getAnimationState(){ return animationState; }
+
+    public void jump(){
+        if (!isInAir){
+            velY = -20f;
+            isInAir = true;
+        }
+    }
+    public void duck(){
+        if (isInAir){
+            velY = 40f;
+        }
+    }
+    public void left(){ velX = -20f; }
+    public void right(){ velX = 20f; }
+
+    public boolean isInAir(){ return isInAir; }
+    public boolean isFalling(){ return isFalling; }
+    public boolean isDucking(){ return isDucking; }
+
+    public void setInAir(boolean isInAir) { this.isInAir = isInAir; }
+    public void setFalling(boolean isFalling) { this.isFalling = isFalling; }
+    public void setDucking(boolean isDucking) { this.isDucking = isDucking; }
+}
+
+interface PlayerAnimationState {
+    String IDLE = "Idle";
+    String JUMPING = "Jumping";
+    String FALLING = "Falling";
+    String PUNCH = "Punch";
 }
