05635a98f1913bd885140584c486a8d1246b9d54
Helvetica-Standard <genangeliludovico26+helvetica@gmail.com>
2026-04-20 19:25:14 +0200

    feat(Attack): Know from who arrived the attack

--- src/model/Attack.java
+++ src/model/Attack.java
@@ -1,19 +1,28 @@
 package model;
 
 public class Attack {
+    private final Character who;
     public double x, y;
+    public double offx, offy;
     public Rect hitbox;
-    public double damage;
+    public final int damage;
     private int duration;
 
-    public Attack(double x, double y, Rect hitbox, double damage, int duration){
-        this.x = x;
-        this.y = y;
-        this.hitbox = hitbox;
-        this.damage = damage;
+    public Attack(Character who, double offx, double offy, Rect hitbox, int damage, int duration){
+        this.who      = who;
+        this.x        = who.x;
+        this.y        = who.y;
+        this.offx     = offx;
+        this.offy     = offy;
+        this.hitbox   = hitbox;
+        this.damage   = damage;
         this.duration = duration;
     }
 
+    public void advance(){
+        x = who.x + offx;
+        y = who.y + offy;
+        duration--;
+    }
     public boolean isAlive(){ return duration > 0; }
-    public void decrease() { duration -= 1; }
 }
