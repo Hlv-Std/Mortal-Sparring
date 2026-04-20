345b3c46b86b541705ad8f33d4981bd58497dfb1
Helvetica-Standard <genangeliludovico26+helvetica@gmail.com>
2026-04-20 11:00:45 +0200

    feat: main menu class

--- src/view/MainMenuPanel.java
+++ src/view/MainMenuPanel.java
@@ -1,11 +1,37 @@
 package view;
 
 import javax.swing.*;
+import java.awt.*;
 
 /*
  * TODO: Show brief video at the start
  * TODO: Change panel upon pressing a button
  */
 public class MainMenuPanel extends JPanel {
-    public MainMenuPanel(){}
+    private JLabel gameName;
+    private JLabel inputText;
+
+    public MainMenuPanel(){
+        super(new GridBagLayout());
+
+        setFocusable(true);
+        requestFocusInWindow();
+
+        gameName = new JLabel("Mortal-Sparring");
+        gameName.setSize(400, 400);
+        var cGameName = new GridBagConstraints();
+        {
+            cGameName.gridx = 1;
+            cGameName.gridy = 0;
+        }
+
+        inputText = new JLabel("Premi un tasto per iniziare");
+        var cInputText = new GridBagConstraints();
+        {
+            cInputText.gridx = 1;
+            cInputText.gridy = 1;
+        }
+        add(gameName, cGameName);
+        add(inputText, cInputText);
+    }
 }
